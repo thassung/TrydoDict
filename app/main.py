4b1895606247557b0d285a00781f5535abd9ddb0
thassung <105700459+thassung@users.noreply.github.com>
2024-02-05 23:00:07 +0700

    init

--- app/main.py
+++ app/main.py
@@ -1,125 +1,314 @@
 from flask import Flask, render_template, request, send_file, send_from_directory, jsonify
-import torch
+import torch, torchdata, torchtext
 import torch.nn as nn
-import torchtext
+import torch.nn.functional as F
+import random, math, time
 import pickle
-import math
 
 app = Flask(__name__, template_folder='templates')
 
-class LSTMLanguageModel(nn.Module):
-    def __init__(self, vocab_size, emb_dim, hid_dim, num_layers, dropout_rate):
-        super().__init__()
-        self.num_layers = num_layers
-        self.hid_dim    = hid_dim
-        self.emb_dim    = emb_dim
-        
-        self.embedding  = nn.Embedding(vocab_size, emb_dim)
-        self.lstm       = nn.LSTM(emb_dim, hid_dim, num_layers=num_layers, dropout=dropout_rate, batch_first=True)
-        self.dropout    = nn.Dropout(dropout_rate)
-        self.fc         = nn.Linear(hid_dim, vocab_size)
-        
-        self.init_weights()
-    
-    def init_weights(self):
-        init_range_emb = 0.1
-        init_range_other = 1/math.sqrt(self.hid_dim)
-        self.embedding.weight.data.uniform_(-init_range_emb, init_range_other)
-        self.fc.weight.data.uniform_(-init_range_other, init_range_other)
-        self.fc.bias.data.zero_()
-        for i in range(self.num_layers):
-            self.lstm.all_weights[i][0] = torch.FloatTensor(self.emb_dim,
-                self.hid_dim).uniform_(-init_range_other, init_range_other) #We
-            self.lstm.all_weights[i][1] = torch.FloatTensor(self.hid_dim,   
-                self.hid_dim).uniform_(-init_range_other, init_range_other) #Wh
-    
-    def init_hidden(self, batch_size, device):
-        hidden = torch.zeros(self.num_layers, batch_size, self.hid_dim).to(device)
-        cell   = torch.zeros(self.num_layers, batch_size, self.hid_dim).to(device)
-        return hidden, cell
-        
-    def detach_hidden(self, hidden):
-        hidden, cell = hidden
-        hidden = hidden.detach() #not to be used for gradient computation
-        cell   = cell.detach()
-        return hidden, cell
-        
-    def forward(self, src, hidden):
-        #src: [batch_size, seq len]
-        embedding = self.dropout(self.embedding(src)) #harry potter is
-        #embedding: [batch-size, seq len, emb dim]
-        output, hidden = self.lstm(embedding, hidden)
-        #ouput: [batch size, seq len, hid dim]
-        #hidden: [num_layers * direction, seq len, hid_dim]
-        output = self.dropout(output)
-        prediction =self.fc(output)
-        #prediction: [batch_size, seq_len, vocab_size]
-        return prediction, hidden
-
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-# model = LSTMLanguageModel(vocab_size, emb_dim, hid_dim, num_layers, dropout_rate).to(device)
-model = LSTMLanguageModel(14204, 1024, 1024, 3, 0.65).to(device)
-model.load_state_dict(torch.load('../model/lstm_gutenberg_best.pt',  map_location=device))
 
-vocab = pickle.load(open('../vocab/vocab_gutenberg.pkl', 'rb'))
+class EncoderLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, pf_dim, dropout, device):
+        super().__init__()
+        self.self_attn_layer_norm = nn.LayerNorm(hid_dim)
+        self.ff_layer_norm        = nn.LayerNorm(hid_dim)
+        self.self_attention       = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
+        self.feedforward          = PositionwiseFeedforwardLayer(hid_dim, pf_dim, dropout)
+        self.dropout              = nn.Dropout(dropout)
 
-tokenizer = torchtext.data.utils.get_tokenizer('basic_english')
+    def forward(self, src, src_mask):
+        src     = self.self_attn_layer_norm(src + self.dropout(_src))
+        _src    = self.feedforward(src)
+        src     = self.ff_layer_norm(src + self.dropout(_src))
+        return src
 
-def generate(prompt, max_seq_len, temperature, model, tokenizer, vocab, device, seed=None):
-    if seed is not None:
-        torch.manual_seed(seed)
-    model.eval()
-    tokens = tokenizer(prompt)
-    indices = [vocab[t] for t in tokens]
-    batch_size = 1
-    hidden = model.init_hidden(batch_size, device)
-    with torch.no_grad():
-        for i in range(max_seq_len):
-            src = torch.LongTensor([indices]).to(device)
-            prediction, hidden = model(src, hidden)
-            
-            #prediction: [batch size, seq len, vocab size]
-            #prediction[:, -1]: [batch size, vocab size] #probability of last vocab
-            
-            probs = torch.softmax(prediction[:, -1] / temperature, dim=-1)  
-            prediction = torch.multinomial(probs, num_samples=1).item()    
-            
-            while prediction == vocab['<unk>']: #if it is unk, we sample again
-                prediction = torch.multinomial(probs, num_samples=1).item()
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hid_dim, n_layers, n_heads, pf_dim, dropout, device, max_length = 512):
+        super().__init__()
+        self.device = device
+        self.tok_embedding = nn.Embedding(input_dim, hid_dim)
+        self.pos_embedding = nn.Embedding(max_length, hid_dim)
+        self.layers        = nn.ModuleList([EncoderLayer(hid_dim, n_heads, pf_dim, dropout, device)
+                                           for _ in range(n_layers)])
+        self.dropout       = nn.Dropout(dropout)
+        self.scale         = torch.sqrt(torch.FloatTensor([hid_dim])).to(self.device)
 
-            if prediction == vocab['<eos>']:    #if it is eos, we stop
-                break
+    def forward(self, src, src_mask):
+        batch_size = src.shape[0]
+        src_len    = src.shape[1]
+        pos        = torch.arange(0, src_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+        src        = self.dropout((self.tok_embedding(src) * self.scale) + self.pos_embedding(pos))
+        for layer in self.layers:
+            src = layer(src, src_mask)
+        return src
 
-            indices.append(prediction) #autoregressive, thus output becomes input
+class MultiHeadAttentionLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, dropout, device):
+        super().__init__()
+        assert hid_dim % n_heads == 0
+        self.hid_dim  = hid_dim
+        self.n_heads  = n_heads
+        self.head_dim = hid_dim // n_heads
 
-    itos = vocab.get_itos()
-    tokens = [itos[i] for i in indices]
-    return tokens
+        self.fc_q     = nn.Linear(hid_dim, hid_dim)
+        self.fc_k     = nn.Linear(hid_dim, hid_dim)
+        self.fc_v     = nn.Linear(hid_dim, hid_dim)
+
+        self.fc_o     = nn.Linear(hid_dim, hid_dim)
+
+        self.dropout  = nn.Dropout(dropout)
+
+        self.scale    = torch.sqrt(torch.FloatTensor([self.head_dim])).to(device)
+
+    def forward(self, query, key, value, mask = None):
+        #src, src, src, src_mask
+        #query = [batch size, query len, hid dim]
+        #key = [batch size, key len, hid dim]
+        #value = [batch size, value len, hid dim]
+
+        batch_size = query.shape[0]
+
+        Q = self.fc_q(query)
+        K = self.fc_k(key)
+        V = self.fc_v(value)
+        #Q=K=V: [batch_size, src len, hid_dim]
+
+        Q = Q.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        K = K.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        V = V.view(batch_size, -1, self.n_heads, self.head_dim).permute(0, 2, 1, 3)
+        #Q = [batch_size, n heads, query len, head_dim]
+
+        energy = torch.matmul(Q, K.permute(0, 1, 3, 2)) / self.scale
+        #Q = [batch_size, n heads, query len, head_dim] @ K = [batch_size, n heads, head_dim, key len]
+        #energy = [batch_size, n heads, query len, key len]
+
+        #for making attention to padding to 0
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, -1e10)
+
+        attention = torch.softmax(energy, dim = -1)
+        #attention = [batch_size, n heads, query len, key len]
+
+        x = torch.matmul(self.dropout(attention), V)
+        #[batch_size, n heads, query len, key len] @ [batch_size, n heads, value len, head_dim]
+        #x = [batch_size, n heads, query len, head dim]
+
+        x = x.permute(0, 2, 1, 3).contiguous()  #we can perform .view
+        #x = [batch_size, query len, n heads, head dim]
+
+        x = x.view(batch_size, -1, self.hid_dim)
+        #x = [batch_size, query len, hid dim]
+
+        x = self.fc_o(x)
+        #x = [batch_size, query len, hid dim]
+
+        return x, attention
+
+class PositionwiseFeedforwardLayer(nn.Module):
+    def __init__(self, hid_dim, pf_dim, dropout):
+        super().__init__()
+        self.fc1 = nn.Linear(hid_dim, pf_dim)
+        self.fc2 = nn.Linear(pf_dim, hid_dim)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        #x = [batch size, src len, hid dim]
+        x = self.dropout(torch.relu(self.fc1(x)))
+        x = self.fc2(x)
+
+        return x
+
+class DecoderLayer(nn.Module):
+    def __init__(self, hid_dim, n_heads, pf_dim, dropout, device):
+        super().__init__()
+        self.self_attn_layer_norm = nn.LayerNorm(hid_dim)
+        self.enc_attn_layer_norm  = nn.LayerNorm(hid_dim)
+        self.ff_layer_norm        = nn.LayerNorm(hid_dim)
+        self.self_attention       = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
+        self.encoder_attention    = MultiHeadAttentionLayer(hid_dim, n_heads, dropout, device)
+        self.feedforward          = PositionwiseFeedforwardLayer(hid_dim, pf_dim, dropout)
+        self.dropout              = nn.Dropout(dropout)
+
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+
+        #trg = [batch size, trg len, hid dim]
+        #enc_src = [batch size, src len, hid dim]
+        #trg_mask = [batch size, 1, trg len, trg len]
+        #src_mask = [batch size, 1, 1, src len]
+
+        _trg, _ = self.self_attention(trg, trg, trg, trg_mask)
+        trg     = self.self_attn_layer_norm(trg + self.dropout(_trg))
+        #trg = [batch_size, trg len, hid dim]
+
+        _trg, attention = self.encoder_attention(trg, enc_src, enc_src, src_mask)
+        trg             = self.enc_attn_layer_norm(trg + self.dropout(_trg))
+        #trg = [batch_size, trg len, hid dim]
+        #attention = [batch_size, n heads, trg len, src len]
+
+        _trg = self.feedforward(trg)
+        trg  = self.ff_layer_norm(trg + self.dropout(_trg))
+        #trg = [batch_size, trg len, hid dim]
+
+        return trg, attention
+
+class Decoder(nn.Module):
+    def __init__(self, output_dim, hid_dim, n_layers, n_heads,
+                 pf_dim, dropout, device,max_length = 512):
+        super().__init__()
+        self.device = device
+        self.tok_embedding = nn.Embedding(output_dim, hid_dim)
+        self.pos_embedding = nn.Embedding(max_length, hid_dim)
+        self.layers        = nn.ModuleList([DecoderLayer(hid_dim, n_heads, pf_dim, dropout, device)
+                                            for _ in range(n_layers)])
+        self.fc_out        = nn.Linear(hid_dim, output_dim)
+        self.dropout       = nn.Dropout(dropout)
+        self.scale         = torch.sqrt(torch.FloatTensor([hid_dim])).to(device)
+
+    def forward(self, trg, enc_src, trg_mask, src_mask):
+
+        #trg = [batch size, trg len]
+        #enc_src = [batch size, src len, hid dim]
+        #trg_mask = [batch size, 1, trg len, trg len]
+        #src_mask = [batch size, 1, 1, src len]
+
+        batch_size = trg.shape[0]
+        trg_len    = trg.shape[1]
+
+        pos = torch.arange(0, trg_len).unsqueeze(0).repeat(batch_size, 1).to(self.device)
+        #pos: [batch_size, trg len]
+
+        trg = self.dropout((self.tok_embedding(trg) * self.scale) + self.pos_embedding(pos))
+        #trg: [batch_size, trg len, hid dim]
+
+        for layer in self.layers:
+            trg, attention = layer(trg, enc_src, trg_mask, src_mask)
+
+        #trg: [batch_size, trg len, hid dim]
+        #attention: [batch_size, n heads, trg len, src len]
+
+        output = self.fc_out(trg)
+        #output = [batch_size, trg len, output_dim]
+
+        return output, attention
+
+class Seq2SeqTransformer(nn.Module):
+    def __init__(self, encoder, decoder, src_pad_idx, trg_pad_idx, device):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_pad_idx = src_pad_idx
+        self.trg_pad_idx = trg_pad_idx
+        self.device = device
+
+    def make_src_mask(self, src):
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        return src_mask
+
+    def make_trg_mask(self, trg):
+        trg_pad_mask = (trg != self.trg_pad_idx).unsqueeze(1).unsqueeze(2)
+        trg_len = trg.shape[1]
+        trg_sub_mask = torch.tril(torch.ones((trg_len, trg_len), device = self.device)).bool()
+        trg_mask = trg_pad_mask & trg_sub_mask
+        return trg_mask
+
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+        enc_src = self.encoder(src, src_mask)
+        output, attention = self.decoder(trg, enc_src, trg_mask, src_mask)
+        return output, attention
+    
+vocab_transform = pickle.load(open('../vocab/vocab_transform.pkl', 'rb'))
+token_transform = pickle.load(open('../vocab/token_transform.pkl', 'rb'))
+
+input_dim   = len(vocab_transform['en'])
+output_dim  = len(vocab_transform['th'])
+hid_dim = 256
+enc_layers = 3
+dec_layers = 3
+enc_heads = 8
+dec_heads = 8
+enc_pf_dim = 512
+dec_pf_dim = 512
+enc_dropout = 0.1
+dec_dropout = 0.1
+
+SRC_PAD_IDX = 1
+TRG_PAD_IDX = 1
+
+enc = Encoder(input_dim,
+              hid_dim,
+              enc_layers,
+              enc_heads,
+              enc_pf_dim,
+              enc_dropout,
+              device)
+
+dec = Decoder(output_dim,
+              hid_dim,
+              dec_layers,
+              dec_heads,
+              dec_pf_dim,
+              enc_dropout,
+              device)
+
+model = Seq2SeqTransformer(enc, dec, SRC_PAD_IDX, TRG_PAD_IDX, device).to(device)
+model.load_state_dict(torch.load('../model/MT_enth.pt',  map_location=device))
 
 @ app.route('/', methods=['POST', 'GET'])
 def main():
     if request.method == 'GET':
         return render_template('index.html')
 
-@ app.route('/chat', methods=['POST', 'GET'])
+@ app.route('/translate', methods=['POST', 'GET'])
 def predict():
     if request.method == 'GET':
         return render_template('index.html')
     if request.method == 'POST':
         inputData = request.json
-        prompt = inputData['prompt'] 
-        temperature = float(inputData['temperature'])
-        seed = int(inputData['seed'])
+        input = inputData['input'] 
 
-        print('>>>>> thinking <<<<<')
+        print('>>>>> calling a translator <<<<<')
 
-        max_seq_len = 30
-        generation = generate(prompt, max_seq_len, temperature, model, tokenizer, 
-                          vocab, device, seed)
-        generated = ' '.join(generation)
-        print('     '+generated+'\n')
-        return jsonify({'generated': generated})
+        src_mapping = vocab_transform['en'].get_itos()
+        trg_mapping = vocab_transform['th'].get_itos()
+
+        src_tokens = ['<sos>'] + token_transform['en'](input) + ['<eos>']
+        src_indexes = []
+        for token in src_tokens:
+            try:
+                src_indexes.append(src_mapping.index(token))
+            except:
+                src_indexes.append(src_mapping.index('<unk>'))
+        print(src_indexes)
+
+        src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+        src_mask = model.make_src_mask(src_tensor)
+
+        with torch.no_grad():
+            enc_src = model.encoder(src_tensor, src_mask)  
+            trg_indexes = [trg_mapping.index('<sos>')] 
+            
+            for i in range(100):  # Assume max length of the target sentence is 100
+                trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+                trg_mask = model.make_trg_mask(trg_tensor)
+                
+                output, attention = model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+                pred_token = output.argmax(2)[:, -1].item()
+                trg_indexes.append(pred_token)  # Add predicted token to trg_indexes
+                
+                if pred_token == trg_mapping.index('<eos>'):
+                    break
+
+        trg_tokens = [trg_mapping[i] for i in trg_indexes]
+
+        translated = ' '.join(trg_tokens[1:-1])
+
+        return jsonify({'translated': translated,
+                        'attention': attention})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=True)
 
